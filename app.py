01d1d6e701383853bf3129ab9ed119a0f2e48ced
matt <mrallinson@gmail.com>
2022-03-30 12:09:20 +0100

    hardcodes stations IDs

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ app= Flask(__name__)
 
 @app.route("/")
 def index():
+	'''calls the TFL API, requests times for inbound and outbound trains,
+	and then sends the times to render_template'''
 	
-	inbound = tracker.train_finder('Wood Street','Liverpool Street')
-	homebound = tracker.train_finder('Liverpool Street', 'Chingford')
+	inbound = tracker.train_finder('910GWDST', #Wood Street
+		                           '910GLIVST' #Liverpool Street
+		                           )
+
+	homebound = tracker.train_finder('910GLIVST', #Liverpool Street
+	                                 '910GCHINGFD' #Chingford
+	                                 )
 
 	return render_template("index.html", inbound=inbound, homebound=homebound)
 
